fd66283da4358bc58399635cedd961725370f2bc
Sowndiriya <sowndhupapa47@gmail.com>
2025-09-28 14:08:38 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    for(int i=1;i<=5;i++){
+      for(int j=5;j>i;j--){
+        System.out.print(" ");
+      }
+      for(int k=1;k<=2*i-1;k++){
+      System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
